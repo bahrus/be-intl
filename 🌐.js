063fc1a4e2d27167a5d8908c6f2ff7912b181697
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-08-09 18:48:11 -0400

    0.0.44

--- 🌐.js
+++ 🌐.js
@@ -1,5 +1,11 @@
+// @ts-check
 import { MountObserver, seed, BeHive } from 'be-hive/be-hive.js';
 import { emc as baseEMC } from './behivior.js';
+/** @import {EMC} from './ts-refs/trans-render/be/types.d.ts' */
+
+/**
+ * @type {EMC}
+ */
 export const emc = {
     ...baseEMC,
     base: '🌐',
